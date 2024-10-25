b3420ee6c81e86f2cb1297db047e0bd03940bb47
RenniOG <rstewarr04@gmail.com>
2024-10-24 22:35:13 -0400

    Fixed entrypoints, PYPI build

--- setup.py
+++ setup.py
@@ -3,6 +3,10 @@ from setuptools import setup, find_packages
 setup(
     name='png_bank',
     version='0.1',
+    description='Password manager that fits into a png',
+    license='MIT',
+    author='Ryan Stewart',
+    author_email='rstewarr04@gmail.com',
     packages=find_packages(),
     install_requires=[
         'pyperclip',
@@ -11,7 +15,7 @@ setup(
     ],
     entry_points={
         'console_scripts': [
-            'my_project=my_project.main:run',  # Command to run the main script
+            'png_bank=png_bank.cli:run',  # Command to run the main script
         ],
     },
 )
